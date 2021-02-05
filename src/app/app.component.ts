939bd72229906845cef6e18a1a187b397050e142
Reynell Bobihis <Reynell.Bobihis@ing.com>
2021-02-05 14:46:45 +0800

    Added src

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'spotifymixmo';
+export class AppComponent  {
+  name = 'Angular';
+  closeMenu() {
+    document.body.classList.remove('mobile-menu-active');
+  }
 }
